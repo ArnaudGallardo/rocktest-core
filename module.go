5e8d513d97949ff1a8d4ab79f6bfdeeb0f77e6f3
Arnaud-Marie Gallardo <arnaud-marie.gallardo@thermofisher.com>
2021-12-06 15:50:22 +0100

    fix: check adding same plugin twice

--- module.go
+++ module.go
@@ -1,5 +1,10 @@
 package core
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Module struct {
 	plugins map[string]func(map[string]interface{}, *Scenario) error
 }
@@ -9,10 +14,15 @@ func (module *Module) GetPluginModule(name string) (func(map[string]interface{},
 	return v, found
 }
 
-func (module *Module) AddPluginModule(name string, v func(map[string]interface{}, *Scenario) error) {
+func (module *Module) AddPluginModule(name string, v func(map[string]interface{}, *Scenario) error) error {
 	if module.plugins == nil {
 		// Init map
 		module.plugins = make(map[string]func(map[string]interface{}, *Scenario) error)
 	}
+	_, found := module.plugins[name]
+	if found {
+		return errors.New(fmt.Sprintf("A plugin module named %s is already instanciated.", name))
+	}
 	module.plugins[name] = v
+	return nil
 }
